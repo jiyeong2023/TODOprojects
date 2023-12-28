147ceb6458bb3429200b541cc51c4b52e2243d63
jiyeong2023 <y974348@gmail.com>
2023-12-28 18:07:31 +0900

    초기 프로젝트 내용없음

---
+++ src/main/kotlin/com/teamsparta/todoprojects/TodOprojectsApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.todoprojects
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodOprojectsApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodOprojectsApplication>(*args)
+}
---
+++ src/test/kotlin/com/teamsparta/todoprojects/TodOprojectsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.todoprojects
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodOprojectsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
