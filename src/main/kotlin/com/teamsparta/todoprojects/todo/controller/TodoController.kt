3de3d292480afe3480d75d3318b67cb2d9a807c1
jiyeong2023 <152155627+jiyeong2023@users.noreply.github.com>
2024-01-02 21:49:56 +0900

    Update TodoController.kt

--- src/main/kotlin/com/teamsparta/todoprojects/todo/controller/TodoController.kt
+++ src/main/kotlin/com/teamsparta/todoprojects/todo/controller/TodoController.kt
@@ -12,15 +12,51 @@ import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/todo")
 @RestController
-class TodoController (
+class TodoController(
     private val todoService: TodoService
-)
-{
+) {
+
+    @GetMapping
+    fun getTodoList(): ResponseEntity<List<TodoResponse>> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(todoService.getAllTodoList())
+    }
+
+    @GetMapping("/{todoId}")
+    fun getTodo(@PathVariable todoId: Long): ResponseEntity<TodoResponse> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(todoService.getTodoByID(todoId))
+    }
+    @ExceptionHandler(ModelNotFoundException::class)
+    fun handleModelNotFoundException(e: ModelNotFoundException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse(e.message))
+    }
+
     @PostMapping
-    fun createTodo(@RequestBody createtodoRequest: CreateTodoRequest): ResponseEntity <TodoResponse> {
+    fun createTodo(@RequestBody createTodoRequest: CreateTodoRequest): ResponseEntity<TodoResponse> {
         return ResponseEntity
             .status(HttpStatus.CREATED)
-            .body(todoService.createTodo(createtodoRequest))
+            .body(todoService.createTodo(createTodoRequest))
+    }
+
+    @PutMapping("/{todoId}")
+    fun updateTodo(
+        @PathVariable todoId: Long,
+        @RequestBody updateTodoRequest: UpdateTodoRequest
+    ): ResponseEntity<TodoResponse> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(todoService.updateTodo(todoId, updateTodoRequest))
+    }
+
+    @DeleteMapping("/{todoId}")
+    fun deleteTodo(@PathVariable todoId: Long): ResponseEntity<Unit> {
+        todoService.deleteTodo(todoId)
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .build()
     }
 
 }
