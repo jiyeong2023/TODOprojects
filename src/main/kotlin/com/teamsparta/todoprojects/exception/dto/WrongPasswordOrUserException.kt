21b6877ac124fd6ef59fbdb7de92b06375a9b85a
t2023-m0027 <y974348@gmail.com>
2024-01-04 23:30:56 +0900

    comment 파일, 리드미 API 수정, todo.kt에 추가로 코드작성필요함.

--- src/main/kotlin/com/teamsparta/todoprojects/exception/dto/WrongPasswordOrUserException.kt
+++ src/main/kotlin/com/teamsparta/todoprojects/exception/dto/WrongPasswordOrUserException.kt
@@ -1,7 +1,6 @@
 package com.teamsparta.todoprojects.exception.dto
 
-import org.springframework.data.jpa.domain.AbstractPersistable_.id
 
-class WrongPasswordOrUserException(val modelName: String, val id: Long?):
-    RuntimeException("Wrong password or user for $modelName with given id: $id") {
+class WrongPasswordOrUserException(val modelName: String,):
+    RuntimeException("Wrong password or user for $modelName with given id:  $commentId") {
 }
